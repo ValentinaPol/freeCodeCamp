c614e74d69a912b8f2907f1c7412d208881c79f7
ValentinaPol <polianskaya.valentina@gmail.com>
2022-12-24 22:46:13 +0300

    Returned value in Function

---
+++ JS/56.js
@@ -0,0 +1,22 @@
+/*let sum = 0;
+
+function addSum(num) {
+  sum = sum + num;
+}
+
+console.log(addSum(3));
+
+console.log(sum);*/
+
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+function addFive() {
+  sum = sum + 5;
+}
+
+addThree();
+addFive();
