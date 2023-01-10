0f473169344a980ec368b8b143a3fa288ca09ae7
ValentinaPol <polianskaya.valentina@gmail.com>
2023-01-11 00:13:45 +0300

    Positive and Negative Lookahead

---
+++ Regular_Expressions_JS/29.js
@@ -0,0 +1,5 @@
+/*Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits. */
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{5,})(?=\D\w*\d{2})/; 
+let result = pwRegex.test(sampleWord);
