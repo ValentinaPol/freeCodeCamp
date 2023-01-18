810abe7f2d350105f19e0c13af9b8b4929a4d162
ValentinaPol <polianskaya.valentina@gmail.com>
2023-01-19 00:26:52 +0300

    Spread Operator to Evaluate Arrays In-Place

---
+++ JS/120.js
@@ -0,0 +1,14 @@
+/*var arr = [6, 89, 3, 45];
+var maximus = Math.max.apply(null, arr);*/
+
+//Second case
+/*const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr);*/ //...arr returns an unpacked array
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  
+
+console.log(arr2);
+
