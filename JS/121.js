1917aa1af2f57763826a71f21161a8de5f13ca76
ValentinaPol <polianskaya.valentina@gmail.com>
2023-01-20 23:37:28 +0300

    Use Destructuring Assignment to Extract Values from Objects

---
+++ JS/121.js
@@ -0,0 +1,8 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+    
+const {today} = HIGH_TEMPERATURES; // equal const today = HIGH_TEMPERATURES.today;
+const {tomorrow} = HIGH_TEMPERATURES;
